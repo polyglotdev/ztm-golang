83f143d46e522830e96c153cb02fc8b028061d88
Dom Hallan <dom@domhallan.com>
2023-11-15 11:55:00 -0500

    🎆 complete arrays exercise

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,31 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	Name  string
+	Price float64
+}
 
+// getTotal takes in an array and returns the total price
+// of all the products in the array
+func getTotal(products [4]Product) float64 {
+	var total float64
+	for _, product := range products {
+		total += product.Price
+	}
+	return total
+}
+
+func main() {
+	var shoppingList [4]Product
+	shoppingList[0] = Product{Name: "Milk", Price: 2.99}
+	shoppingList[1] = Product{Name: "Eggs", Price: 1.99}
+	shoppingList[2] = Product{Name: "Bread", Price: 1.99}
+	shoppingList[3] = Product{Name: "Butter"}
+	li := len(shoppingList) - 1
+	ti := len(shoppingList)
+	total := getTotal(shoppingList)
+	fmt.Println("Last item:", shoppingList[li].Name)
+	fmt.Println("Total items:", ti)
+	fmt.Println("Total cost:$", fmt.Sprintf("%.2f", total))
 }
