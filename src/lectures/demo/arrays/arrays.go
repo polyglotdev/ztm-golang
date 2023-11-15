e592e768a3f653c4ba99b0db69ba6696290e2c6d
Dom Hallan <dom@domhallan.com>
2023-11-15 11:38:09 -0500

    🎆 complete arrays demo

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	Name    string
+	Cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.Cleaned {
+			fmt.Println(room.Name, "is clean")
+		} else {
+			fmt.Println(room.Name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{Name: "Office"},
+		{Name: "Warehouse"},
+		{Name: "Reception"},
+		{Name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+	fmt.Println("Perform cleaning...")
+	rooms[2].Cleaned = true
+	rooms[3].Cleaned = true
+
+	checkCleanliness(rooms)
 }
