501e76a9a1ef8c2cfa817ec755c3a1ed70db166a
Dom Hallan <dom@domhallan.com>
2023-11-14 20:29:25 -0500

    🎆 initial commit

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -1,12 +1,35 @@
-//--Summary:
-//  Copy your rcv-func solution to this directory and write unit tests.
+// --Summary:
 //
-//--Requirements:
-//* Write unit tests that ensure:
-//  - Health & energy can not go above their maximums
-//  - Health & energy can not go below 0
-//* If any of your  tests fail, make the necessary corrections
-//  in the copy of your rcv-func solution file.
+//	Copy your rcv-func solution to this directory and write unit tests.
 //
-//--Notes:
-//* Use `go test -v ./exercise/testing` to run these specific tests
+// --Requirements:
+// * Write unit tests that ensure:
+//   - Health & energy can not go above their maximums
+//   - Health & energy can not go below 0
+//   - If any of your  tests fail, make the necessary corrections
+//     in the copy of your rcv-func solution file.
+//
+// --Notes:
+// * Use `go test -v ./exercise/testing` to run these specific tests
+package main
+
+import (
+	"testing"
+)
+
+func TestHealth(t *testing.T) {
+	// Test that health can not go above 100
+	p := Player{}
+	p.health = 100
+	p.heal(10)
+	if p.health != 100 {
+		t.Errorf("Health should not go above 100")
+	}
+
+	// Test that health can not go below 0
+	p.health = 0
+	p.damage(10)
+	if p.health != 0 {
+		t.Errorf("Health should not go below 0")
+	}
+}
